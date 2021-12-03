0d0b76a143287d97131df9769af742e32d93621b
eagarwal@ieee.org <E@garwal7>
2021-12-02 18:27:50 -0600

    remove smac

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,9 +1,9 @@
 from envs.predatorprey_wrapper import PredatorPreyWrapper
-from envs.smac_wrapper import SMACWrapper
-from envs.traffic_junction_wrapper import TrafficJunctionWrapper
+# from envs.smac_wrapper import SMACWrapper
+# from envs.traffic_junction_wrapper import TrafficJunctionWrapper
 
 __all__ = [
     'PredatorPreyWrapper',
-    'SMACWrapper',
-    'TrafficJunctionWrapper',
+    # 'SMACWrapper',
+    # 'TrafficJunctionWrapper',
 ]
