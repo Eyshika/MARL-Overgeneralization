54c94a5d200f62ad4a5a57204170685510fe89a8
Eyshika Agarwal <eagarwal@ieee.org>
2021-12-04 06:04:58 +0000

    fix

--- predatorprey/dec_predatorprey_runner.py
+++ predatorprey/dec_predatorprey_runner.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 current_file_path = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(current_file_path + '/../../')
+sys.path.append(current_file_path + '/../')
 
 import socket
 import collections
